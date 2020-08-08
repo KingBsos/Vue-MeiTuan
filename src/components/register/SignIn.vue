edd59727d99a54fde0ddfafbe39ec0bbc155c503
KingBsos <kingbsos2@hotmail.com>
2020-08-08 17:35:15 +0800

    l

--- src/components/register/SignIn.vue
+++ src/components/register/SignIn.vue
@@ -1,30 +1,29 @@
 <template>
   <div>
-      <div class="container text-muted font-size-12">
-          <div class="w-50 page-left">
-              <a href="//index"><img class="logo" src="https://s0.meituan.net/bs/file/?f=fe-sso-fs:build/assets/logo.6a89007.png"></a>
-              <img src="https://s0.meituan.net/bs/file/?f=fe-sso-fs:build/page/static/banner/www.jpg">
-              <div class="my-2em">
-                  <a class="d-ib p-sm" href="#aksdfj">关于美团</a>
-                  <a class="p-sm d-ib" href="#aksdfj">加入我们</a>
-                  <a class="p-sm d-ib" href="#aksdfj">商家入驻</a>
-                  <a class="p-sm d-ib" href="#aksdfj">帮助中心</a>
-                  <a class="p-sm d-ib" href="#aksdfj">美团手机版</a>
-              </div>
-              <p>©2020 美团网团购 meituan.com 京ICP证070791号 京公网安备11010502025545号</p>
-          </div>
-          <div class="w-50 page-right">
-              <div class="input-container">
-                  <span>账号登录</span>
-                  <a>手机动态码登陆</a>
-                  <input class="input" type="text" placeholder="手机号" v-model="accountNum">
-                  <input class="input" type="password" placeholder="密码" v-model="password">
-                  <p>忘记密码?</p>
-                  <a href="#">登陆</a>
-                  <p>还没有账号?免费注册</p>
-                  <div></div>
-              </div>
-          </div>
+      <div class="left">
+        <a href="//index">
+            <img class="logo-pic" src="https://s0.meituan.net/bs/file/?f=fe-sso-fs:build/assets/logo.6a89007.png">
+        </a>
+        <img src="https://s0.meituan.net/bs/file/?f=fe-sso-fs:build/page/static/banner/www.jpg">
+        <div class="">
+            <a class="d-ib p-sm" href="#aksdfj">关于美团</a>
+            <a class="p-sm d-ib" href="#aksdfj">加入我们</a>
+            <a class="p-sm d-ib" href="#aksdfj">商家入驻</a>
+            <a class="p-sm d-ib" href="#aksdfj">帮助中心</a>
+            <a class="p-sm d-ib" href="#aksdfj">美团手机版</a>
+        </div>
+        <p>©2020 美团网团购 meituan.com 京ICP证070791号 京公网安备11010502025545号</p>
+      </div><div class="right">
+          <div class="input-container">
+                <span>账号登录</span>
+                <a>手机动态码登陆</a>
+                <input class="input" type="text" placeholder="手机号" v-model="accountNum">
+                <input class="input" type="password" placeholder="密码" v-model="password">
+                <p>忘记密码?</p>
+                <a href="#">登陆</a>
+                <p>还没有账号?免费注册</p>
+                <div></div>
+            </div>
       </div>
   </div>
 </template>
@@ -41,7 +40,16 @@ export default {
 </script>
 
 <style scoped>
-.container {
+.left, .right {
+    display: inline-block;
+    width: 50vw;
+    height: 100vh;
+    padding: 50px;
+}
+.logo-pic {
+    width: 100%;
+}
+/* .container {
     margin: auto;
     max-width: 1200px;
     padding: 20px;
@@ -60,5 +68,5 @@ export default {
     width: 100%;
     padding: 5px 20px;
     border: 1px solid #ddd;
-}
+} */
 </style>
